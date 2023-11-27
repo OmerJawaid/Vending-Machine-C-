17564861159fbc39708aa620e410aa0c6abcf0ac
OmerJawaid <omerjawaid0@gmail.com>
2023-11-27 21:36:13 +0500

    Issues: quantity decrementign to 0 does not vanishes the item from menu. re and re executing until user enters420.

--- Vending Machine/Vending Machine.cpp
+++ Vending Machine/Vending Machine.cpp
@@ -7,167 +7,112 @@ int items_price(int i);
 
 int main()
 {
-	int main_choice,item_index[10],item_count = 0, item_quantity[10], item_prices[10];
+	int main_choice,item_index[10],product_count = 0, product_ava_array[10], item_quantity[10], item_prices[10], item_input_count,purchsae_selection[10],purchase_qunatity;
 	char stop;
 	string item_name[10];
 	do
 	{
 		char item_count_choice,add_more_item_choice{};
 		//Base item Adding
-		if (item_count == 0)
+		if (product_count == 0)
 		{
 			cout << "Do you want to add items (Y/N)";
 			cin >> item_count_choice;
 			do {
-				if (item_count_choice == 'Y'|| item_count_choice == 'y')
-				{	
-					int item_input_count;
+				if (item_count_choice == 'Y' || item_count_choice == 'y')
+				{
+
 					cout << "How many Items do you want to add?";
 					cin >> item_input_count;
-					if (item_input_count <= 6)
+					if (item_input_count <= 10)
 					{
-						switch (item_input_count)
+						for (int i = 1; i <= item_input_count; i++)
 						{
-						case 1:
-							item_name[1] = items_name(1);
-							item_quantity[1] = items_quantity(1);
-							item_prices[1] = items_price(1);
-							item_index[1]=1;
-							item_count += 1;
-							break;
-						case 2:
-							for (int i = 1; i <= 2; i++)
+							item_name[i] = items_name(i);
+							item_quantity[i] = items_quantity(i);
+							item_prices[i] = items_price(i);
+							item_index[i] = i;
+							if (item_quantity[i] != 0)
 							{
-								item_name[i] = items_name(i);
-								item_quantity[i] = items_quantity(i);
-								item_prices[i] = items_price(i);
-								item_index[i] = i;
+								product_ava_array[i] = 1;
 							}
-							item_count += 2;
-							break;
-						case 3:
-							for (int i = 1; i <= 3; i++)
-							{
-								item_name[i] = items_name(i);
-								item_quantity[i] = items_quantity(i);
-								item_prices[i] = items_price(i);
-								item_index[i] = i;
-							}
-							item_count += 3;
-							break;
-						case 4:
-							for (int i = 1; i <= 4; i++)
-							{
-								item_name[i] = items_name(i);
-								item_quantity[i] = items_quantity(i);
-								item_prices[i] = items_price(i);
-								item_index[i] = i;
-							}
-							item_count += 4;
-							break;
-						case 5:
-							for (int i = 1; i <= 5; i++)
-							{
-								item_name[i] = items_name(i);
-								item_quantity[i] = items_quantity(i);
-								item_prices[i] = items_price(i);
-								item_index[i] = i;
-							}
-							item_count += 5;
-							break;
-						case 6:
-							for (int i = 1; i <= 6; i++)
-							{
-								item_name[i] = items_name(i);
-								item_quantity[i] = items_quantity(i);
-								item_prices[i] = items_price(i);
-								item_index[i] = i;
-							}
-							item_count += 6;
-							break;
 						}
+							product_count+=item_input_count;
+						
 					}
 					else
-						cout << "Max items to input are 6."<<endl;
+						cout << "Max items to input are 10."<<endl;
 				}
 				else
 					cout << "There are no items to be purchased. Add some!" << endl;
+				
 				cout << "Do you want to add items? (Y/N)";
 				cin >> item_count_choice;
-			} while (item_count==0); //item_count_choice != 'N' && item_count_choice != 'n'
-		}
+				
+					//Menu
+					for (int i = 1; i <= item_input_count; i++)
+					{
+						if (product_ava_array[i] == 1)
+						{
+							cout << i << ". " << item_name[i] << ":  " << item_prices[i] << endl;
+						}
+					}
 
-		//Menu
-		cout << "1. Restocking" << endl
-			<< "2. Purchase" << endl
-			<< "3.Quit Menu" << endl
-			<< "What do you want to do?";
-		cin >> main_choice;
-		//Avalible Items
-		
-		//Restocking
-		do {
-			if (main_choice == 1)
-			{
-				char restock_choice;
-				do {
-					int item_search, adding_quantity;
-					cout << "Which Item you want to restock?\nEnter the index" << endl;
-					cin >> item_search;
-					for (int i = 1; i <= 6; i++)
-					{
-						if (item_search == i)
+					//Purchase
+					do {
+
+						cout << "Enter the number to purchase that item: ";
+						cin >> main_choice;
+						if (main_choice == 101)
 						{
-							cout << item_name[i] << " " << item_quantity[i] << endl;
-							cout << "Enter the quantity to add: ";
-							cin >> adding_quantity;
-							item_quantity[i] += adding_quantity;
-							cout << "New Quantity: " << item_quantity[i] << endl
-								<< "Item Restocked" << endl;
-							break;
-						}
-					}
-					cout << "Do you want to restock more products?(Y/N)";
-					cin >> restock_choice;
-				} while (restock_choice != 'N' && restock_choice != 'n');
-			}
-			//Purchasing
-			if (main_choice == 2)
-			{
-				char purchase;
-				do {
-					int item_search, price_notax, tax_price, price_tax;
-					cout << "Enter the index of item you want to purchase: ";
-					cin >> item_search;
-					for (int i = 1; i <= 6; i++)
-					{
-						if (item_search == i)
-						{
-							int sale_quantity;
-							cout << item_name[i] << " " << item_prices[i] << endl;
-							if (item_quantity == 0)
+							cout << "How many Items do you want to add?";
+							cin >> item_input_count;
+							if (item_input_count <= 10)
 							{
-								cout << "The product is not in stock" << endl;
-								break;
+								for (int i = 1; i <= item_input_count; i++)
+								{
+									item_name[i] = items_name(i);
+									item_quantity[i] = items_quantity(i);
+									item_prices[i] = items_price(i);
+									item_index[i] = i;
+									if (item_quantity[i] != 0)
+									{
+										product_ava_array[i] = 1;
+									}
+								}
+								product_count += item_input_count;
+
 							}
-							cout << "Enter the quantity: ";
-							cin >> sale_quantity;
-							price_notax = sale_quantity * item_prices[i];
-							tax_price = price_notax * 0.14;
-							price_tax = price_notax - tax_price;
-							item_quantity[i]--;
-							cout << "Price: " << price_tax;
-							cout << "Item Purchased Successfully." << endl;
-							break;
+							else
+								cout << "Max items to input are 10." << endl;
 						}
+						else if (main_choice == 1 || main_choice == 2 || main_choice == 3 || main_choice == 4 || main_choice == 5 || main_choice == 6 || main_choice == 7 || main_choice == 8 || main_choice == 9 || main_choice == 10)
+						{
+							cout << "Quantity: ";
+							cin >> purchase_qunatity;
+							int count = 0;
+							for (int i = 1; i <= item_input_count; i++)
+							{
+								int j = 1, price_notax, tax;
+								if (i == main_choice)
+								{
+									price_notax = item_prices[i] * purchase_qunatity;
+									tax = price_notax * 0.14;
+									cout << j << ". " << item_name[i] << ":  " << price_notax + tax;
+									j++;
+									count++;
+									item_quantity[i] = item_quantity[i] - purchase_qunatity;
+								}
+							}
+							if (count == 0)
+								cout << "Item not avalible.";
+						}
+						else
+							cout << "Try again!";
 					}
-					cout << "Do you want to make another purchase? ";
-					cin >> purchase;
-				} while (purchase != 'N' && purchase != 'n');
-			}
-			else
-				cout << "Enter correct choice.";
-		} while (main_choice != 3);
+					while (main_choice != 420);
+			} while (product_count==0); //item_count_choice != 'N' && item_count_choice != 'n'
+		}
 		cout << "If you want to quit press Q else press any alphabet.";
 		cin >> stop;
 	} while (stop != 'Q' && stop != 'q');
